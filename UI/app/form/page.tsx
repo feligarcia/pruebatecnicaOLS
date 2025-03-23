32c7b437ef61f16e41d354108b49b67ab95c9922
Juan Felipe Garcia Gil <jufeg2@gmail.com>
2025-03-22 21:39:11 -0500

    Se agrega formik y yup para validacion de formularios

--- UI/app/form/page.tsx
+++ UI/app/form/page.tsx
@@ -1,31 +1,76 @@
+"use client";
+import { ErrorMessage, useFormik } from "formik";
+import { useState } from "react";
+import { object, string, date } from "yup";
+
 function page() {
+  const [isLoading, setIsLoading] = useState(false);
+  const formik = useFormik({
+    initialValues: {
+      id: "",
+      nombre: "",
+      municipio: "",
+      telefono: "",
+      correo: "",
+      fecharegistro: "",
+      estado: "",
+    },
+    validationSchema: object({
+      nombre: string().required("El nombre es requerido"),
+      municipio: string().required("El municipio es requerido"),
+      telefono: string().required("El telefono es requerido"),
+      correo: string()
+        .email("Correo invalido")
+        .required("El correo es requerido"),
+      fecharegistro: date().required("La fecha de registro es requerida"),
+      estado: string().oneOf(["activo", "inactivo"]).required(),
+    }),
+    onSubmit: async (formData, { resetForm }) => {
+      setIsLoading(true);
+      console.log("Datos enviados:", formData);
+      resetForm();
+      setIsLoading(false);
+    },
+  });
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex border-b border-gray-300 bg-blue-50">
         <h2 className="text-lg font-bold text-blue-900 p-4 ">Empresa 1</h2>
       </div>
       <div className="bg-blue-200 flex-1">
-        <form className="flex flex-col shadow-lg bg-white mx-20 rounded-xl border border-gray-400 my-10">
+        <form
+          className="flex flex-col shadow-lg bg-white mx-20 rounded-xl border border-gray-400 my-10"
+          onSubmit={formik.handleSubmit}
+          id="form"
+        >
           <div className="flex border-b border-gray-300">
             <h2 className="text-lg font-bold text-blue-900 p-4 ">
-              Datos generales
+              Datos generales {isLoading && "Cargando..."}
             </h2>
           </div>
           <div className="text-center flex flex-row py-10 align-center justify-between">
             <div className="flex flex-col gap-6  border-r border-gray-400  justify-start flex-1/2 px-8">
               <div className="relative w-full">
                 <label
-                  htmlFor="razonsocial"
+                  htmlFor="nombre"
                   className="absolute -top-3 left-3 text-sm font-medium text-gray-700  bg-white px-1"
                 >
                   Razón social
                 </label>
                 <input
-                  id="razonsocial"
+                  required
+                  id="nombre"
                   type="text"
                   className="w-full px-4 py-2 border border-gray-600 rounded-md text-black focus:ring-2 focus:ring-pink-600"
-                  // onChange={(e) => setDocument(e.target.value)}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.nombre}
                 />
+                {formik.touched.nombre && formik.errors.nombre && (
+                  <div className="text-red-500 text-sm">
+                    {formik.errors.nombre}
+                  </div>
+                )}
               </div>
               <div className="relative w-full">
                 <label
@@ -35,39 +80,62 @@ function page() {
                   Departamento
                 </label>
                 <input
+                  required
                   id="departamento"
                   type="text"
                   className="w-full px-4 py-2 border border-gray-600 rounded-md text-black focus:ring-2 focus:ring-pink-600"
-                  // onChange={(e) => setDocument(e.target.value)}
+                  //   onChange={formik.handleChange}
+                  //   onBlur={formik.handleBlur}
+                  //   value={formik.values.departamento}
                 />
+                {/* {formik.touched.nombre && formik.errors.nombre && (
+                  <div className="text-red-500 text-sm">
+                    {formik.errors.nombre}
+                  </div>
+                )} */}
               </div>
               <div className="relative w-full">
                 <label
-                  htmlFor="ciudad"
+                  htmlFor="municipio"
                   className="absolute -top-3 left-3 text-sm font-medium text-gray-700  bg-white px-1"
                 >
-                  Ciudad
+                  Municipio
                 </label>
                 <input
-                  id="ciudad"
+                  required
+                  id="municipio"
                   type="text"
                   className="w-full px-4 py-2 border border-gray-600 rounded-md text-black focus:ring-2 focus:ring-pink-600"
-                  // onChange={(e) => setDocument(e.target.value)}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.municipio}
                 />
+                {formik.touched.municipio && formik.errors.municipio && (
+                  <div className="text-red-500 text-sm">
+                    {formik.errors.municipio}
+                  </div>
+                )}
               </div>
               <div className="relative w-full">
                 <label
                   htmlFor="telefono"
                   className="absolute -top-3 left-3 text-sm font-medium text-gray-700  bg-white px-1"
                 >
-                  telefono
+                  Teléfono
                 </label>
                 <input
                   id="telefono"
                   type="text"
                   className="w-full px-4 py-2 border border-gray-600 rounded-md text-black focus:ring-2 focus:ring-pink-600"
-                  // onChange={(e) => setDocument(e.target.value)}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.telefono}
                 />
+                {formik.touched.telefono && formik.errors.telefono && (
+                  <div className="text-red-500 text-sm">
+                    {formik.errors.telefono}
+                  </div>
+                )}
               </div>
             </div>
             <div className="flex flex-col gap-6  flex-1/2 px-8">
@@ -76,37 +144,71 @@ function page() {
                   htmlFor="correo"
                   className="absolute -top-3 left-3 text-sm font-medium text-gray-700  bg-white px-1"
                 >
-                  correo
+                  Correo eléctronico
                 </label>
                 <input
                   id="correo"
                   type="text"
                   className="w-full px-4 py-2 border border-gray-600 rounded-md text-black focus:ring-2 focus:ring-pink-600"
-                  // onChange={(e) => setDocument(e.target.value)}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.correo}
                 />
+                {formik.touched.correo && formik.errors.correo && (
+                  <div className="text-red-500 text-sm">
+                    {formik.errors.correo}
+                  </div>
+                )}
               </div>
               <div className="relative w-full">
                 <label
                   htmlFor="fecharegistro"
                   className="absolute -top-3 left-3 text-sm font-medium text-gray-700  bg-white px-1"
                 >
-                  fecharegistro
+                  Fecha de registro
                 </label>
                 <input
+                  required
                   id="fecharegistro"
-                  type="text"
+                  type="date"
                   className="w-full px-4 py-2 border border-gray-600 rounded-md text-black focus:ring-2 focus:ring-pink-600"
-                  // onChange={(e) => setDocument(e.target.value)}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.fecharegistro}
                 />
+                {formik.touched.fecharegistro &&
+                  formik.errors.fecharegistro && (
+                    <div className="text-red-500 text-sm">
+                      {formik.errors.fecharegistro}
+                    </div>
+                  )}
               </div>
+              <div className="relative w-full">
+                <label
+                  htmlFor="estado"
+                  className="absolute -top-3 left-3 text-sm font-medium text-gray-700 bg-white px-1"
+                >
+                  Estado
+                </label>
+                <select
+                  id="estado"
+                  className="w-full px-4 py-2 border border-gray-600 rounded-md text-black focus:ring-2 focus:ring-pink-600"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.estado}
+                >
+                  <option value="activo">Activo</option>
+                  <option value="inactivo">Inactivo</option>
+                </select>
+                {formik.touched.estado && formik.errors.estado && (
+                  <div className="text-red-500 text-sm">
+                    {formik.errors.estado}
+                  </div>
+                )}
+              </div>
+
               <div className="flex items-center gap-2 text-left">
-                <input
-                  type="checkbox"
-                  id="conestableci"
-                  // checked={estable}
-                  // onChange={() => setAcceptedTerms(!estable)}
-                  className="w-4 h-4"
-                />
+                <input type="checkbox" id="conestableci" className="w-4 h-4" />
                 <label htmlFor="conestableci" className="text-sm text-gray-800">
                   Posee establecimientos?
                 </label>
@@ -114,6 +216,30 @@ function page() {
             </div>
           </div>
         </form>
+      </div>
+      <div className="flex flex-row align-center justify-center px-9 bg-blue-200">
+        <div className="flex flex-row flex-1 align-center justify-center text-white h-20 bg-blue-900 rounded-t-xl mt-20 gap-4 space-x-1 px-4">
+          <div className="flex flex-col justify-center">
+            <p className="text-white text-sm">Total ingresos formulario:</p>
+            <p className=" text-blue-300 font-bold">$100.000.000</p>
+          </div>
+          <div className="flex flex-col justify-center">
+            <p className="text-white text-sm">Cantidad empleados:</p>
+            <p className=" text-blue-300 font-bold">999</p>
+          </div>
+          <div className="flex flex-row flex-1 border border-gray-600 rounded-md gap-1 justify-center align-center my-4">
+            <p className="block text-white text-xs m-auto p-2">
+              Si ya ingresaste todos los datos, crea tu formulario aquí:
+            </p>
+            <button
+              className="bg-pink-600 text-white border border-gray-600 rounded-md p-1 px-2 cursor-pointer text-sm flex-1 h-6/10 my-auto mr-3 hover:opacity-90"
+              type="submit"
+              form="form"
+            >
+              Enviar Formulario
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
